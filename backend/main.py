6fd242ca8c96246fe772c9be947dc25e86980548
Ryuthns <s6301012620049@email.kmutnb.ac.th>
2023-10-11 18:34:18 +0700

    set port 8000

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from typing import Union
 
 from fastapi import FastAPI, File, UploadFile
@@ -21,3 +22,6 @@ async def create_upload_file(file: UploadFile):
     c = TestClassification()
     result = c.predict(bytefile=file.file)
     return result
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
