02db553425c4ede4ee0857b5c3f67ab5ddac0562
Thanasak-s63 <s6301012620049@email.kmutnb.ac.th>
2023-10-18 22:05:14 +0700

    add CORS

--- backend/classification.py
+++ backend/classification.py
@@ -1,12 +1,93 @@
-# sample execution (requires torchvision)
+import os
 from PIL import Image
 from torchvision import transforms
 import torch
 import io
-from typing import BinaryIO, Union
+from typing import BinaryIO, Tuple, Union, List
+from torch.utils.data import DataLoader, TensorDataset
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms
 
 class TestClassification:
+    def __init__(self, num_classes: int):
+        self.model = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', pretrained=True)
+        self.criterion = nn.CrossEntropyLoss()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model.to(self.device)
+        # Freeze all layers
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        # Modify the last layer to have the number of output classes as specified
+        in_features = self.model.classifier[1].in_features  # Number of input features to the final layer
+        self.model.classifier[1] = nn.Linear(in_features, num_classes)  # Replace the final layer
+
+        for param in self.model.classifier[1].parameters():
+            param.requires_grad = True
+
+
+    def train(self, bytefiles:Union[List[BinaryIO], None], labels: List[int], epochs:int, lr:float):
+        if bytefiles is None or labels is None:
+            return
+
+        preprocess = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+        images = []
+        for bf in bytefiles:
+            image = Image.open(io.BytesIO(bf.read()))
+            input_tensor = preprocess(image)
+            images.append(input_tensor)
+
+        train_dataset = TensorDataset(torch.stack(images), torch.Tensor(labels).long())
+        train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+        
+        optimizer = optim.SGD(self.model.parameters(), lr=lr)
+
+        epoch_losses = []
+        epoch_accuracies = []
+        for epoch in range(epochs):
+            running_loss = 0.0
+            correct = 0
+            total = 0
+
+            for i, data in enumerate(train_loader, 0):
+                inputs, label = data
+                optimizer.zero_grad()
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, label)
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+
+                _, predicted = torch.max(outputs.data, 1)
+                total += label.size(0)
+                correct += (predicted == label).sum().item()
+
+            epoch_loss = running_loss / len(train_loader)
+            epoch_losses.append(epoch_loss)
+
+            epoch_accuracy = 100 * correct / total
+            epoch_accuracies.append(epoch_accuracy)
+
+            print(f"Epoch {epoch+1}, Loss: {epoch_loss}, Accuracy: {epoch_accuracy}%")
+
+        torch.save(self.model.state_dict(), 'mobilenet_v2_trained.pth')
+        print("Finished Training and saved the model")
+
+        return {'loss': epoch_losses, 'accuracy': epoch_accuracies}
+
+
     def predict(self, bytefile:Union[BinaryIO, None]=None, filename=""):
+        if os.path.exists('mobilenet_v2_trained.pth'):
+            self.model.load_state_dict(torch.load('mobilenet_v2_trained.pth'))
+        
         input_image = []
         if bytefile != None:
             input_image = Image.open(io.BytesIO(bytefile.read()))
@@ -15,8 +96,7 @@ class TestClassification:
 
         if input_image == []:
             return {}
-        model = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', pretrained=True)
-        model.eval()
+        self.model.eval()
         preprocess = transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
@@ -29,27 +109,57 @@ class TestClassification:
 # move the input and model to GPU for speed if available
         if torch.cuda.is_available():
             input_batch = input_batch.to('cuda')
-            model.to('cuda')
+            self.model.to('cuda')
 
         with torch.no_grad():
-            output = model(input_batch)
+            output = self.model(input_batch)
 # Tensor of shape 1000, with confidence scores over ImageNet's 1000 classes
         print(output[0])
 # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
         probabilities = torch.nn.functional.softmax(output[0], dim=0)
         print(probabilities)
         # Read the categories
-        with open("imagenet_classes.txt", "r") as f:
-            categories = [s.strip() for s in f.readlines()]
-        # Show top categories per image
-        top5_prob, top5_catid = torch.topk(probabilities, 5)
-        c = {}
-        for i in range(top5_prob.size(0)):
-            c[categories[top5_catid[i]]] = top5_prob[i].item()
-        return c
+        if not os.path.exists('mobilenet_v2_trained.pth'):
+            c = {}
+            with open("imagenet_classes.txt", "r") as f:
+                categories = [s.strip() for s in f.readlines()]
+            # Show top categories per image
+            top5_prob, top5_catid = torch.topk(probabilities, 5)
 
+            for i in range(top5_prob.size(0)):
+                c[categories[top5_catid[i]]] = top5_prob[i].item()
+            
+            return c
+        # Creating a dictionary of class indices and their respective probabilities
+        class_prob_dict = {}
+        for idx in range(len(probabilities)):
+            class_prob_dict[idx] = probabilities[idx].item()
+
+        return class_prob_dict
+
+def load_images_from_folder(folder_path: str) -> (List[BinaryIO], List[int]):
+    bytefiles = []
+    labels = []
+    label_mapping = {'Cat': 0, 'Dog': 1}
+
+    for label_name, label_idx in label_mapping.items():
+        dir_path = os.path.join(folder_path, label_name)
+        for filename in os.listdir(dir_path):
+            if filename.endswith('.png'):
+                img_path = os.path.join(dir_path, filename)
+                with open(img_path, 'rb') as f:
+                    byte_content = f.read()
+                    bytefiles.append(io.BytesIO(byte_content))
+                    labels.append(label_idx)
+    
+    return bytefiles, labels
 
 if __name__ == "__main__":
-    c = TestClassification()
-    result = c.predict(filename="./images/images.jpg")
-    print(result)
+    folder_path = "Dog and Cat .png"
+    bytefiles, labels = load_images_from_folder(folder_path)
+    #
+    classifier = TestClassification(2)
+    result = classifier.train(bytefiles, labels, epochs=3, lr=0.001)
+    predicted = classifier.predict(filename='./images/images.jpg')
+    # print(result)
+    print(predicted)
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,23 @@
 import uvicorn
-from typing import Union
+from typing import Union, List
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.middleware.cors import CORSMiddleware
 from classification import TestClassification
 
 app = FastAPI()
 
+origins = [
+   "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -19,8 +31,26 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
-    c = TestClassification()
+    c = TestClassification(10)
     result = c.predict(bytefile=file.file)
+    print(result)
+    return result
+
+@app.post("/train/")
+async def train_model(
+    bytefiles: List[UploadFile] = File(...),
+    labels: List[int] = Form(...),
+    epochs: int = Form(...),
+    lr: float = Form(...)
+):
+    # Convert UploadFile objects to BinaryIO
+    bytefile_data = [bf.file for bf in bytefiles]
+    
+    # Call the train method with the received data
+    c = TestClassification(10)
+    result = c.train(bytefile_data, labels, epochs, lr)
+
+    # Return the result as JSON
     return result
 
 if __name__ == "__main__":
